ceaa69b721d15845ec19bf0d81f7590eaac8de8e
denisgorlachev <117777771+denisgorlachev@users.noreply.github.com>
2022-11-08 23:08:41 +0200

    Initial commit

---
+++ лаба 1.1/Program.cs
@@ -0,0 +1,10 @@
+﻿//Горлачев высокий уровень 
+Console.WriteLine("Введите x: ");
+double x = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите y: ");
+double y = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите a: ");
+double a = double.Parse(Console.ReadLine());
+double G = 0;
+G = Math.Pow(Math.Cos(Math.Abs(y + x)), 2) - (x + y) / Math.Pow(Math.Atan(x + a), 4) * Math.Pow(x, 5);
+Console.WriteLine("G равен:"+G);    
